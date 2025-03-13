65f4ff7113dd29d0b6ab931fab38afea8a642038
Founders Approach <35708450+FoundersApproach@users.noreply.github.com>
2025-03-13 15:42:54 +0530

    Update acquisition.ts

--- api/script/routes/acquisition.ts
+++ api/script/routes/acquisition.ts
@@ -182,13 +182,10 @@ export function getAcquisitionRouter(config: AcquisitionConfig): express.Router 
 
           let _server_url = process.env["SERVER_URL"]
           console.log("_server_url", _server_url)
-          
-          const ipRegex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/;
-          const match = _server_url.match(ipRegex);
-          _server_url = match ? match[0] : null;
 
-          
-          updateCheckBody.updateInfo.downloadURL = updateCheckBody.updateInfo.downloadURL.replace('127.0.0.1', _server_url);
+          _server_url = updateCheckBody.updateInfo.downloadURL.replace("127.0.0.1", _server_url);
+
+          updateCheckBody.updateInfo.downloadURL = _server_url
           
           console.log("updateCheckBody",updateCheckBody.updateInfo)
             
